93f4a36056bac8af2ae497d43fcabaeef4443bfb
blumenbeet1 <161455626+blumenbeet1@users.noreply.github.com>
2026-04-19 10:14:07 +0200

    Delete Techentrepreneurship website directory

--- Techentrepreneurship website/pages/startseite.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("Willkommen bei Rent a Hobby")
-
-st.write("Entdecke neue Hobbys und miete sie für kurze Zeit!")
-
-# Hier kannst du weitere Inhalte für die Startseite hinzufügen
